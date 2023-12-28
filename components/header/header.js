982419752787f4379aeb035c23838fc177b6967c
suong1272000 <suong1272000@gmail.com>
2023-12-28 17:55:05 +0900

    feat: 모바일 header 수정

---
+++ components/header/header.js
@@ -0,0 +1,18 @@
+$('hamburger').click(function() {
+    $('nav-left-list').toggleClass('open');
+   });
+  
+// });
+// var isMenuOpened = false;
+// const element = document.getElementsByClassName('.nav-left-list');
+
+// function openMenu() {
+//     console.log(isMenuOpened)
+//     if(isMenuOpened) {
+//         isMenuOpened = false
+//         element.style.display = "none";
+//     } else {
+//         isMenuOpened = true
+//         element.style.display = "block";
+//     }
+// }
